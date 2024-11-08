ba6074e8eae2fbf720e45ce48c32ec4e9c6daada
szilibettina06 <szilibettina06@gmail.com>
2024-11-08 09:09:40 +0100

    Project:ToWriteAngular

---
+++ Mancs_Tars/src/app/shelters/shelters.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shelters',
+  standalone: true,
+  imports: [],
+  templateUrl: './shelters.component.html',
+  styleUrl: './shelters.component.css'
+})
+export class SheltersComponent {
+
+}
