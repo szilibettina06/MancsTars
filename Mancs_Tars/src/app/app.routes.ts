7ec2b44cfb69112d357d3d3492838f435c0000a4
szilibettina06 <szilibettina06@gmail.com>
2024-11-11 11:31:49 +0100

    ToWriteAngular:MainPage1

--- Mancs_Tars/src/app/app.routes.ts
+++ Mancs_Tars/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: MainPageComponent }
+    
+];
