5bae6e51bdf545586a85640e304c103eaf0466ba
szilibettina06 <szilibettina06@gmail.com>
2024-12-02 11:14:35 +0100

    delete angular project

--- Mancs_Tars/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { MainPageComponent } from './main-page/main-page.component';
-
-export const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: MainPageComponent }
-    
-];
