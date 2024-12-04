a71b1abfae435079b507ba6f49ef78ab0740a26d
rkcziviktria <145556398+rkcziviktria@users.noreply.github.com>
2024-12-04 11:11:05 +0100

    UserController

---
+++ Mancs_Tars/src/main/java/com/mancs_tars/controller/UserController.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/GenericResource.java to edit this template
+ */
+package com.mancs_tars.controller;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author rkczi
+ */
+@Path("user")
+public class UserController {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of UserController
+     */
+    public UserController() {
+    }
+
+    /**
+     * Retrieves representation of an instance of com.mancs_tars.controller.UserController
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public String getXml() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of UserController
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_XML)
+    public void putXml(String content) {
+    }
+}
